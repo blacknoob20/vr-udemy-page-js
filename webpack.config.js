d487f706df579d4d3861525f9097d33c9265a5c3
cristian.guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-09 14:00:21 -0500

    Agrego la conficuracion de los paquetes html-loader y html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          minimize: false,
+        },
+      },
+    ],
+  },
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin()],//[new HtmlWebpackPlugin({template:'../scr/index.html', filename:''})]
 };
