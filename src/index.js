d7b136c811695773508688baf4d0d8a4356ec715
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-11 00:04:04 -0500

    Configuracion para crear el archivo css en la carpeta dist/ y poner la llamada en el html de la pagina

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Importar módulos, no es obligatiorio poner la extension porque por defecto lo hace.
 import { saludar } from './js/componentes';
+import './styles.css';
 
 const nombre = 'Cristhian Guerrero';
 saludar(nombre);
