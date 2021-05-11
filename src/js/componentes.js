d0ab4cc9386a73e512778152df00ed8118aafdbc
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-10 23:29:11 -0500

    Importo los estilos como si fuera un archivo javascript

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,4 +1,6 @@
-const saludar = (nombre) =>{
+import '../css/componentes.css';
+
+export const saludar = (nombre) =>{
     console.log('Creando etiqueta H1');
 
     const h1 = document.createElement('h1');
