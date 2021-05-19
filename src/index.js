2b894c3316c67951605d4f24a398ba5c0923c3ca
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-19 09:43:48 -0500

    Agregando Post dinamicamente en nuestra pagina web

--- src/index.js
+++ src/index.js
@@ -2,6 +2,37 @@ import './styles.css';
 import "../node_modules/bxslider/dist/jquery.bxslider.css";
 
 // Initialize the slider
-  $(document).ready(() => {
-    $('.bxslider').bxSlider();
-  });
+$('.bxslider').bxSlider();
+
+const posts = [
+    {
+        title: 'Titulo del post 1',
+        date: `Publicado el: ${new Date().toLocaleDateString()}`,
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, repellendus. Deleniti magnam, ut delectus asperiores mollitia totam quaerat voluptates, blanditiis iusto vero quas! Officia at nihil provident enim quod voluptatem!',
+    },
+    {
+        title: 'Titulo del post 2',
+        date: `Publicado el: ${new Date().toLocaleDateString()}`,
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, repellendus. Deleniti magnam, ut delectus asperiores mollitia totam quaerat voluptates, blanditiis iusto vero quas! Officia at nihil provident enim quod voluptatem!',
+    },
+    {
+        title: 'Titulo del post 3',
+        date: `Publicado el: ${new Date().toLocaleDateString()}`,
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, repellendus. Deleniti magnam, ut delectus asperiores mollitia totam quaerat voluptates, blanditiis iusto vero quas! Officia at nihil provident enim quod voluptatem!',
+    }
+];
+
+const div = document.createElement('div');
+
+posts.forEach(post => {
+    div.innerHTML += `
+    <article class="post">
+    <h2>${post.title}</h2>
+    <span class="date">${post.date}</span>
+    <p>${post.content}</p>
+    <a href="#" class="button-more">Leer mas</a>
+    </article>
+    `;
+});
+
+document.querySelector('#posts').append(div);
